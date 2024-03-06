4b82628243b0112990d8cc1ec534471e21b5ec7d
uhuikim <uhui94@naver.com>
2024-03-06 22:33:49 +0900

    data-grid!!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,84 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import 'react-data-grid/lib/styles.css';
+
+import DataGrid, { RenderCellProps, RenderEditCellProps, RenderRowProps, Row, textEditor } from 'react-data-grid';
+import { useState } from 'react';
+
+type RowType = {
+  name: string,
+  account: string,
+  password: number,
+  errorMessage?: string,
+  [key: string]: string | number | undefined
+}
+
+const Test = (props: RenderCellProps<RowType>) => {
+  const { row, column } = props
+  const { key } = column
+
+  console.log(row)
+
+  if (row.errorMessage) {
+    return <div style={{ background: "yellow" }}>{row[key]}</div>
+  }
+  return <div>{row[key]}</div>
+}
+
+
+const TextEditor = (props: RenderEditCellProps<RowType>) => {
+  const { row, column } = props
+  const { key } = column
+  console.log(props)
+
+  if (row.errorMessage) {
+    return <div style={{ background: "yellow" }}>{row[key]}</div>
+  }
+  return <div>{row[key]}</div>
+}
+
+
+
+const columns = [
+  { key: 'name', name: '이름', renderEditCell: TextEditor, renderCell: Test },
+  { key: 'account', name: '계정', renderEditCell: textEditor, renderCell: Test },
+  { key: 'password', name: '비밀번호', renderEditCell: textEditor, renderCell: Test }
+];
+
+
+
+const rowsData: RowType[] = [
+  { name: '1', account: 'test1@naver.com', password: 1234, errorMessage: '계정 수정해주셈' },
+  { name: '2', account: 'test2@naver.com', password: 3452 },
+  { name: '3', account: 'test3@naver.com', password: 1234234, errorMessage: '계정 수정해주셈' },
+];
+
+
+function myRowRenderer(key: React.Key, props: RenderRowProps<RowType>) {
+  return (
+    <Row {...props} key={key} />
+  );
+}
+
 
 export default function Home() {
+  const [rows, setRows] = useState(rowsData);
+
+  const handleDeleteRow = (rowIdx: number) => {
+    setRows([...rows.slice(0, rowIdx), ...rows.slice(rowIdx + 1)]);
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <DataGrid
+      columns={columns}
+      rows={rows}
+      onRowsChange={setRows}
+      renderers={{ renderRow: myRowRenderer }}
+      onCellContextMenu={({ row }, event) => {
+        event.preventGridDefault();
+        console.log(row, event)
+      }}
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    />
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   );
 }
